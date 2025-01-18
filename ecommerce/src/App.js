85f30ae0d6b8b4e58b4a3295871155afd9790113
breno-spartacode <109807534+BSDO@users.noreply.github.com>
2025-01-18 18:36:32 -0300

    INICIALIZAÇÃO DE PROJETO.

---
+++ ecommerce/src/App.js
@@ -0,0 +1,16 @@
+const App =() => {
+
+  return (
+    <div class="grid grid-col-12 ap-4 ">
+      <div className='flex justify-between pt-6 px-10 '>
+          <div className='col-span-4 hidden md:col-span-4'>1</div>
+          <div className='col-span-4'>2</div>
+          <div className='col-span-4'>3</div>
+
+      </div>
+        
+  </div>
+  );
+}
+
+export default App;
