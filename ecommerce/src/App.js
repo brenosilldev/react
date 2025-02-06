23765621462a618e60e92ef6d193d27ba5745abd
breno-spartacode <109807534+BSDO@users.noreply.github.com>
2025-02-06 00:34:04 -0300

    :wastebasket: cleanup: removido classes.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,15 +1,10 @@
+import {Rotas} from "./routes/routes" 
+
+
 const App =() => {
 
   return (
-    <div class="grid grid-col-12 ap-4 ">
-      <div className='flex justify-between pt-6 px-10 '>
-          <div className='col-span-4 hidden md:col-span-4'>1</div>
-          <div className='col-span-4'>2</div>
-          <div className='col-span-4'>3</div>
-
-      </div>
-        
-  </div>
+      <Rotas />
   );
 }
 
